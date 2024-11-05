592426d3d0c9f8a45398af3c6b384b77aeab0de2
carlosAless <carloaless1512@gmail.com>
2024-11-05 12:37:05 -0300

    projeto

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import "./Footer.css";
+import logowhite from "../../assets/logowhite.png";
 
 const Footer = () => {
-  return <footer>&copy;Prime Tecnologia 2015 - 2024</footer>;
+  return (
+    <footer>
+      <div className="logo">
+        <img src={logowhite} alt="logo" srcSet="" />
+      </div>
+      <div className="network">facebook</div>
+      <div className="primetecnologia">&copy;Prime Tecnologia 2015 - 2024</div>
+    </footer>
+  );
 };
 
 export default Footer;
