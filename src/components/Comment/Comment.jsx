1bc0f2ff2ed1c4d4cfdbcb202417b4fa4737e913
carlosAless <carloaless1512@gmail.com>
2024-12-19 11:03:32 -0300

    atualização

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,167 +1,81 @@
 import "./Comment.css";
 
 export const Comment = () => {
+
+  const comments = [
+    {
+      id: 1,
+      name: "Luiz Felipe",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Quero expressar minha gratidão e satisfação pelo excelente serviço de instalação do office em meu notebook. Recomendo!!",
+    },
+    {
+      id: 2,
+      name: "IZABELLA BORGES RODRIGUES COSTA",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Excelente atendimento, ótima qualidade e rapidez.",
+    },
+    {
+      id: 3,
+      name: "Lucas Henrique",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Atendimento rápido, de qualidade e preço justo. Profissionais qualificados e atenciosos.",
+    },
+    {
+      id: 4,
+      name: "Celio Bernardo de Lima",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Tive um problema em um equipamento eletrônico que foi resolvido perfeitamente",
+    },
+    {
+      id: 5,
+      name: "Lucas Henrique",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Atendimento rápido, de qualidade e preço justo. Profissionais qualificados e atenciosos.",
+    },
+    {
+      id: 6,
+      name: "Helder Allucci",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Empresa de tecnologia focada em gestão de acesso e ponto",
+    },
+    {
+      id: 7,
+      name: "Joao Marcos Luca Costa",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Fabuloso serviço",
+    },
+    {
+      id: 8,
+      name: "Willer Silva de Morais",
+      stars: "⭐⭐⭐⭐⭐",
+      text: "Bom atendimento.",
+    },
+  ];
+
+  const infiniteComments = [...comments, ...comments];
+
   return (
-    <>
-      <div className="comments">
-        <h2>O que estão dizendo sobre nós</h2>
-        <div className="wrapper">
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">Luiz Felipe</span>
+    <div className="comments">
+      <h2>O que estão dizendo sobre nós</h2>
+      <div className="wrapper">
+        <div className="comment-track">
+          {infiniteComments.map((comment, index) => (
+            <div className="comment" key={index}>
+              <div className="comment-header">
+                <img
+                  src="https://via.placeholder.com/50"
+                  alt="User Avatar"
+                  className="comment-avatar"
+                />
+                <span className="comment-name">{comment.name}</span>
+              </div>
+              <div className="comment-stars">{comment.stars}</div>
+              <div className="comment-text">{comment.text}</div>
             </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">
-              Quero expressar minha gratidão e satisfação pelo excelente serviço
-              de instalação do office em meu notebook, Recomendo!!
-            </div>
-          </div>
-
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">
-                IZABELLA BORGES RODRIGUES COSTA
-              </span>
-            </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">
-              Excelente atendimento, ótima qualidade e rapidez.
-            </div>
-          </div>
-
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">Lucas Henrique</span>
-            </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">
-              Atendimento rápido, de qualidade e preço justo. Profissionais
-              qualificados e atenciosos.
-            </div>
-          </div>
-
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">Celio Bernardo de Lima</span>
-            </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">
-              Tive um problema em um equipamento eletrônico que foi resolvido
-              perfeitamente
-            </div>
-          </div>
-
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">Helder Allucci</span>
-            </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">
-              Empresa de tecnologia focada em gestão de acesso e ponto
-            </div>
-          </div>
-
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">Joao Marcos Luca Costa</span>
-            </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">Fabuloso serviço</div>
-          </div>
-
-          <div className="comment">
-            {/* Foto e Nome */}
-            <div className="comment-header">
-              <img
-                src="https://via.placeholder.com/50"
-                alt="User Avatar"
-                className="comment-avatar"
-              />
-              <span className="comment-name">Willer Silva de Morais</span>
-            </div>
-
-            {/* Estrelas */}
-            <div className="comment-stars">
-              ⭐⭐⭐⭐⭐ {/* Substitua por lógica dinâmica se necessário */}
-            </div>
-
-            {/* Comentário */}
-            <div className="comment-text">Bom atendimento.</div>
-          </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
